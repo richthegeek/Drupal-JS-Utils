8991b9d53a4300c45155f5a1375946258ef11d1c
Richard Lyon <richthegeek@gmail.com>
2012-04-10 00:05:01 +0100

    First commit

---
+++ jsutils.coffee
@@ -0,0 +1,102 @@
+class Modules
+	constructor: () ->
+		@dependencies = {}
+		if Drupal.settings.dependencies
+			for stack, mods of Drupal.settings.dependencies when Drupal.settings.dependencies.hasOwnProperty(stack)
+				for mod in mods
+					@add_dependency(stack, mod)
+		if Drupal.modules
+			mods = jQuery.extend({}, Drupal.modules)
+			@attach(mod, fns) for mod, fns of mods when mods.hasOwnProperty(mod)
+
+	#######################################################
+	# DEPENDENCY MANAGEMENT
+	#######################################################
+
+	# Initialise a dependency stack such that it fires
+	# the "ready" event after the page has loaded, even
+	# if not all dependencies were resolved.
+	init_dependency: (stack, fallback = true) ->
+		@dependencies[stack] ?= {}
+
+		if fallback
+			callback = () ->
+				if Drupal.modules.dependency_status(stack) != true
+					jQuery(document).trigger(stack + '.ready')
+					# alert('ready ' + stack)
+			jQuery(window).load(callback)
+
+	# Add a dependency to a stack.
+	add_dependency: (stack, name) ->
+		@dependencies[stack] ?= {}
+		@dependencies[stack][name] ?= false
+
+	# Resolve a dependency on all stacks
+	resolve_dependency: (name) ->
+		for stack, mods of @dependencies when @dependencies.hasOwnProperty(stack)
+			if mods[name]?
+				mods[name] = true
+				jQuery(document).trigger(stack + '.update')
+				jQuery(document).trigger(stack + '.ready') if @dependency_status(stack)
+
+	# Check the status of a dependency stack
+	dependency_status: (stack) ->
+		count = 0
+		if @dependencies[stack]
+			for mod, status of @dependencies[stack]
+				count++
+				if @dependencies[stack].hasOwnProperty(mod) and not status
+					return false
+		return (if count then true else 1)
+
+	#######################################################
+	# MODULE MANAGEMENT
+	#######################################################
+
+	# Attach a module to the module list. Recommend using this
+	# method in the event that there is a caching implementation
+	# at a later date.
+	attach: (module, fns) ->
+		if not self[module]? or not self.hasOwnProperty(module)
+			self[module] = fns
+		else
+			console.error('JSUtils - attempt to overwrite a reserved word')
+
+	# Lists modules that implement a hook
+	implements: (hook) ->
+		module for module, fns of Drupal.modules when fns[hook]?
+
+	# Invokes a hook on a named module, returning the result
+	# Returns null if no such module exists.
+	invoke: (module, hook) ->
+		args = Array::slice.call(arguments, 2)
+		if Drupal.modules[module] and Drupal.modules[module][hook]
+			return Drupal.modules[module][hook].call(window, args)
+
+	# Invokes a hook on all modules that implement it, returning
+	# a merged object OR an array of results, hopefully acting
+	# identically to the PHP version of the same name.
+	invoke_all: (hook) ->
+		args = Array::slice.call(arguments, 1)
+		result = []
+		for module in @implements(hook)
+			result_inner = Drupal.modules[module][hook].call(window, args)
+			if result_inner and result_inner.join?
+				result = jQuery.extend(true, result, result_inner)
+			else if result_inner
+				result.push(result_inner)
+		return result
+
+	# Similar to drupal_alter, with some changes.
+	# Incoming data *must* be an object for referencing to work.
+	# Probably requires more sanitation/de-packing on the caller.
+	alter: (hook, data) ->
+		hook = hook + '_alter'
+		for module in @implements(hook)
+			data = Drupal.modules[module][hook].call(window, data)
+		return data
+
+
+Drupal.modules = new Modules()
+jQuery(() ->
+)
---
+++ jsutils.js
@@ -0,0 +1,152 @@
+(function() {
+  var Modules;
+
+  Modules = (function() {
+
+    function Modules() {
+      var fns, mod, mods, stack, _i, _len, _ref;
+      this.dependencies = {};
+      if (Drupal.settings.dependencies) {
+        _ref = Drupal.settings.dependencies;
+        for (stack in _ref) {
+          mods = _ref[stack];
+          if (Drupal.settings.dependencies.hasOwnProperty(stack)) {
+            for (_i = 0, _len = mods.length; _i < _len; _i++) {
+              mod = mods[_i];
+              this.add_dependency(stack, mod);
+            }
+          }
+        }
+      }
+      if (Drupal.modules) {
+        mods = jQuery.extend({}, Drupal.modules);
+        for (mod in mods) {
+          fns = mods[mod];
+          if (mods.hasOwnProperty(mod)) this.attach(mod, fns);
+        }
+      }
+    }
+
+    Modules.prototype.init_dependency = function(stack, fallback) {
+      var callback, _base;
+      if (fallback == null) fallback = true;
+      if ((_base = this.dependencies)[stack] == null) _base[stack] = {};
+      if (fallback) {
+        callback = function() {
+          if (Drupal.modules.dependency_status(stack) !== true) {
+            return jQuery(document).trigger(stack + '.ready');
+          }
+        };
+        return jQuery(window).load(callback);
+      }
+    };
+
+    Modules.prototype.add_dependency = function(stack, name) {
+      var _base, _base2, _ref;
+      if ((_base = this.dependencies)[stack] == null) _base[stack] = {};
+      return (_ref = (_base2 = this.dependencies[stack])[name]) != null ? _ref : _base2[name] = false;
+    };
+
+    Modules.prototype.resolve_dependency = function(name) {
+      var mods, stack, _ref, _results;
+      _ref = this.dependencies;
+      _results = [];
+      for (stack in _ref) {
+        mods = _ref[stack];
+        if (this.dependencies.hasOwnProperty(stack)) {
+          if (mods[name] != null) {
+            mods[name] = true;
+            jQuery(document).trigger(stack + '.update');
+            if (this.dependency_status(stack)) {
+              _results.push(jQuery(document).trigger(stack + '.ready'));
+            } else {
+              _results.push(void 0);
+            }
+          } else {
+            _results.push(void 0);
+          }
+        }
+      }
+      return _results;
+    };
+
+    Modules.prototype.dependency_status = function(stack) {
+      var count, mod, status, _ref;
+      count = 0;
+      if (this.dependencies[stack]) {
+        _ref = this.dependencies[stack];
+        for (mod in _ref) {
+          status = _ref[mod];
+          count++;
+          if (this.dependencies[stack].hasOwnProperty(mod) && !status) {
+            return false;
+          }
+        }
+      }
+      return (count ? true : 1);
+    };
+
+    Modules.prototype.attach = function(module, fns) {
+      if (!(self[module] != null) || !self.hasOwnProperty(module)) {
+        return self[module] = fns;
+      } else {
+        return console.error('JSUtils - attempt to overwrite a reserved word');
+      }
+    };
+
+    Modules.prototype.implements = function(hook) {
+      var fns, module, _ref, _results;
+      _ref = Drupal.modules;
+      _results = [];
+      for (module in _ref) {
+        fns = _ref[module];
+        if (fns[hook] != null) _results.push(module);
+      }
+      return _results;
+    };
+
+    Modules.prototype.invoke = function(module, hook) {
+      var args;
+      args = Array.prototype.slice.call(arguments, 2);
+      if (Drupal.modules[module] && Drupal.modules[module][hook]) {
+        return Drupal.modules[module][hook].call(window, args);
+      }
+    };
+
+    Modules.prototype.invoke_all = function(hook) {
+      var args, module, result, result_inner, _i, _len, _ref;
+      args = Array.prototype.slice.call(arguments, 1);
+      result = [];
+      _ref = this.implements(hook);
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        module = _ref[_i];
+        result_inner = Drupal.modules[module][hook].call(window, args);
+        if (result_inner && (result_inner.join != null)) {
+          result = jQuery.extend(true, result, result_inner);
+        } else if (result_inner) {
+          result.push(result_inner);
+        }
+      }
+      return result;
+    };
+
+    Modules.prototype.alter = function(hook, data) {
+      var module, _i, _len, _ref;
+      hook = hook + '_alter';
+      _ref = this.implements(hook);
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        module = _ref[_i];
+        data = Drupal.modules[module][hook].call(window, data);
+      }
+      return data;
+    };
+
+    return Modules;
+
+  })();
+
+  Drupal.modules = new Modules();
+
+  jQuery(function() {});
+
+}).call(this);
